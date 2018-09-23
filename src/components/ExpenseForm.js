9e1a472b3a0f328241bdf237bed77475a33a7894
protatodev <thaddonaghue@yahoo.com>
2018-09-23 13:47:16 -0700

    remove console log from expenseform component

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -4,7 +4,6 @@ import { SingleDatePicker } from 'react-dates';
 
 
 const now = moment();
-console.log(now.format('MMM Do, YYYY @ h:mm:ss A'));
 
 class ExpenseForm extends React.Component {
   constructor(props) {
